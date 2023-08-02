656869cfc72f33b9edcc92f5bb671193708ad435
Bullets-jyr <jyr@bullets.co.kr>
2023-08-02 13:04:02 +0900

    8. Modules

--- src/main/kotlin/kr/co/bullets/Application.kt
+++ src/main/kotlin/kr/co/bullets/Application.kt
@@ -1,12 +1,25 @@
 package kr.co.bullets
 
 import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
 import kr.co.bullets.plugins.*
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
-}
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
-    configureRouting()
+//    configureRouting()
+    routing {
+        get("/") {
+            call.respondText("Hello, World!")
+        }
+    }
+}
+
+fun Application.module2() {
+    routing {
+        get("/book") {
+            call.respondText("Hello, World 2!")
+        }
+    }
 }
